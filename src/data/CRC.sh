57ebad6302835ea40d4c89bcb85cc322d2ee7a73
nxdens <liw82@pitt.edu>
2020-12-01 11:34:10 -0500

    more debug

--- src/data/CRC.sh
+++ src/data/CRC.sh
@@ -21,6 +21,7 @@ usage() {
     CURDIR="$(pwd)"
     echo $FS_LICENSE
     echo $CURDIR
+    echo $SCRATCH
     # header text
     echo "
 $log_ToolName: Queueing script for running MPP on Slurm-based computing clusters
@@ -28,31 +29,6 @@ $log_ToolName: Queueing script for running MPP on Slurm-based computing clusters
 Usage: $log_ToolName
                     --studyFolder=<path>                Path to folder with subject images
                     --subjects=<path or list>           File path or list with subject IDs
-                    [--class=<3T|7T|T1w_MPR|T2w_SPC>]   Name of the class
-                    [--domainX=<3T|7T|T1w_MPR|T2w_SPC>] Name of the domain X
-                    [--domainY=<3T|7T|T1w_MPR|T2w_SPC>] Name of the domain Y
-                    [--brainSize=<int>]                 Brain size estimate in mm, default 150 for humans
-                    [--windowSize=<int>]                window size for bias correction, default 30.
-                    [--brainExtractionMethod=<RPP|SPP>] Registration (Segmentation) based brain extraction
-                    [--MNIRegistrationMethod=<nonlinear|linear>] Do (not) use FNIRT for image registration to MNI
-                    [--custombrain=<NONE|MASK|CUSTOM>] If you have created a custom brain mask saved as
-                                                       '<subject>/<domainX>/custom_bc_brain_mask.nii.gz', specify 'MASK'.
-                                                       If you have created custom structural images, e.g.:
-                                                       - '<subject>/<domainX>/<domainX>_bc.nii.gz'
-                                                       - '<subject>/<domainX>/<domainX>_bc_brain.nii.gz'
-                                                       - '<subject>/<domainX>/<domainY>_bc.nii.gz'
-                                                       - '<subject>/<domainX>/<domainY>_bc_brain.nii.gz'
-                                                       to be used when peforming MNI152 Atlas registration, specify
-                                                       'CUSTOM'. When 'MASK' or 'CUSTOM' is specified, only the
-                                                        AtlasRegistration step is run.
-                                                        If the parameter is omitted or set to NONE (the default),
-                                                        standard image processing will take place.
-                                                        NOTE: This option allows manual correction of brain images
-                                                        in cases when they were not successfully processed and/or
-                                                        masked by the regular use of the pipelines.
-                                                        Before using this option, first ensure that the pipeline
-                                                        arguments used were correct and that templates are a good
-                                                        match to the data.
                     [--printcom=command]                if 'echo' specified, will only perform a dry run.
 
     PARAMETERs are [ ] = optional; < > = user supplied value
@@ -69,14 +45,6 @@ input_parser() {
 
     opts_AddMandatory '--studyFolder' 'studyFolder' 'raw data folder path' "a required value; is the path to the study folder holding the raw data. Don't forget the study name (e.g. /mnt/storinator/edd32/data/raw/ADNI)"
     opts_AddMandatory '--subjects' 'subjects' 'path to file with subject IDs' "a required value; path to a file with the IDs of the subject to be processed (e.g. /mnt/storinator/edd32/data/raw/ADNI/subjects.txt)" "--subject" "--subjectList" "--subjList"
-    opts_AddOptional  '--class' 'class' 'Class Name' "an optional value; is the name of the class. Default: 3T. Supported: 3T | 7T | T1w_MPR | T2w_SPC" "3T"
-    opts_AddOptional  '--domainX' 'domainX' 'Domain X' "an optional value; is the name of the domain X. Default: T1w_MPR. Supported: 3T | 7T | T1w_MPR | T2w_SPC" "T1w_MPR"
-    opts_AddOptional  '--domainY' 'domainY' 'Domain Y' "an optional value; is the name of the domain Y. Default: T2w_SPC. Supported: 3T | 7T | T1w_MPR | T2w_SPC" "T2w_SPC"
-    opts_AddOptional '--windowSize'  'windowSize' 'window size for bias correction' "an optional value; window size for bias correction; for 7T MRI, the optimal value ranges between 20 and 30. Default: 30." "30"
-    opts_AddOptional '--brainSize' 'brainSize' 'Brain Size' "an optional value; the average brain size in mm. Default: 150." "150"
-    opts_AddOptional '--brainExtractionMethod'  'BrainExtractionMethod' 'Registration (Segmentation) based brain extraction method' "an optional value; The method used to perform brain extraction. Default: RPP. Supported: RPP | SPP." "RPP"
-    opts_AddOptional '--MNIRegistrationMethod'  'MNIRegistrationMethod' '(non)linear registration to MNI' "an optional value; if it is set then only an affine registration to MNI is performed, otherwise, a nonlinear registration to MNI is performed. Default: linear. Supported: linear | nonlinear." "linear"
-    opts_AddOptional '--customBrain'  'CustomBrain' 'If custom mask or structural images provided' "an optional value; If you have created a custom brain mask saved as <subject>/<domainX>/custom_brain_mask.nii.gz, specify MASK. If you have created custom structural images, e.g.: '<subject>/<domainX>/<domainX>_bc.nii.gz - '<subject>/<domainX>/<domainX>_bc_brain.nii.gz - '<subject>/<domainY>/<domainY>_bc.nii.gz - '<subject>/<domainY>/<domainY>_bc_brain.nii.gz' to be used when peforming MNI152 Atlas registration, specify CUSTOM. When MASK or CUSTOM is specified, only the AtlasRegistration step is run. If the parameter is omitted or set to NONE (the default), standard image processing will take place. NOTE: This option allows manual correction of brain images in cases when they were not successfully processed and/or masked by the regular use of the pipelines. Before using this option, first ensure that the pipeline arguments used were correct and that templates are a good match to the data. Default: NONE. Supported: NONE | MASK| CUSTOM." "NONE"
     opts_AddOptional '--printcom' 'RUN' 'do (not) perform a dray run' "an optional value; If RUN is not a null or empty string variable, then this script and other scripts that it calls will simply print out the primary commands it otherwise would run. This printing will be done using the command specified in the RUN variable, e.g., echo" "" "--PRINTCOM" "--printcom"
 
     opts_ParseArguments "$@"
@@ -138,30 +106,6 @@ setup() {
     LOGDIR="${NODEDIR}/logs/"
     mkdir -p $LOGDIR
 
-    # Templates
-
-    # Hi resolution domain X MNI template
-    xTemplate="${MNI_Templates}/MNI152_T1_0.7mm.nii.gz"
-    # Hi resolution brain extracted domain X MNI template
-    xTemplateBrain="${MNI_Templates}/MNI152_T1_0.7mm_brain.nii.gz"
-    # Low resolution domain X MNI template
-    xTemplate2mm="${MNI_Templates}/MNI152_T1_2mm.nii.gz"
-    # Hi resolution domain Y MNI template
-    yTemplate="${MNI_Templates}/MNI152_T2_0.7mm.nii.gz"
-    # Hi resoslution brain extracted domain Y MNI template
-    yTemplateBrain="${MNI_Templates}/MNI152_T2_0.7mm_brain.nii.gz"
-    # Low resolution domain Y MNI template
-    yTemplate2mm="${MNI_Templates}/MNI152_T2_2mm.nii.gz"
-    # Hi resolution MNI brain mask template
-    TemplateMask="${MNI_Templates}/MNI152_T1_0.7mm_brain_mask.nii.gz"
-    # Low resolution MNI brain mask template
-    Template2mmMask="${MNI_Templates}/MNI152_T1_2mm_brain_mask_dil.nii.gz"
-
-    # Other Config Settings
-    # BrainSize in mm, 150 for humans
-    BrainSize="150"
-    # FNIRT 2mm T1w Config
-    FNIRTConfig="${MPP_Config}/T1_2_MNI152_2mm.cnf"
 }
 
 # Join function with a character delimiter
