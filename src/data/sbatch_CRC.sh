b02fa1bb52a95f78a373edb4095565eda9278d10
nxdens <liw82@pitt.edu>
2020-12-01 15:47:32 -0500

    checking the file paths

--- src/data/sbatch_CRC.sh
+++ src/data/sbatch_CRC.sh
@@ -61,6 +61,9 @@ input_parser() {
     mkdir -p "$(dirname "$0")"/logs/slurm
     subjectPath=$(ls -d /pylon5/med200002p/liw82/KLU/90*/80*)
     echo $subjectPath
+    for index in ${!subjectPath[@]}; do
+        echo $index/${#subjectPath[@]}
+    done
     files=187
 
     
