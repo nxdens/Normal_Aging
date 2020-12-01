d262252e6b945a5341b2d280c6e8039fb78de0bc
nxdens <liw82@pitt.edu>
2020-12-01 15:51:51 -0500

    huh

--- src/data/sbatch_CRC.sh
+++ src/data/sbatch_CRC.sh
@@ -60,7 +60,7 @@ input_parser() {
     # Make slurm logs directory
     mkdir -p "$(dirname "$0")"/logs/slurm
     subjectPath=$(ls -d /pylon5/med200002p/liw82/KLU/90*/80*)
-    echo $subjectPath
+    echo $subjectPath[1]
     for index in ${!subjectPath[@]}; do
         echo $index/${#subjectPath[@]}
     done
