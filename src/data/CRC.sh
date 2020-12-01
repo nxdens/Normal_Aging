bac6d8354bf0b3f6fb9ab67d1c34461b4e3e6e78
nxdens <liw82@pitt.edu>
2020-12-01 13:51:45 -0500

    fix imagedir

--- src/data/CRC.sh
+++ src/data/CRC.sh
@@ -62,7 +62,7 @@ setup() {
     # The directory holding the data for the subject correspoinding ot this job
     # pass the path to each scan for each subject to each job -lw
     BASE=$LIW82/KLU/$subjectIDs/$scanIDs
-    IMAGEDIR=$BASE/converted/Hires/${SCANIDs}_Hires.nii
+    IMAGEDIR="$BASE/converted/Hires/${SCANIDs}_Hires.nii"
     
     # Node directory that where computation will take place
     SUBJECTDIR=$BASE/step_01_Freesurfer/
